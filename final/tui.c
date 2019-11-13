c77e494ef7ad6eba2d4e4aae571eb627d1325031
xxc3nsoredxx <xxc3ncoredxx@gmail.com>
2019-11-13 08:57:04 -0600

    Drive selection works
    The menu with the list of detected block devices successfully selects
    the desired drive and inits.

--- final/tui.c
+++ final/tui.c
@@ -1,3 +1,4 @@
+#include <dirent.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +7,7 @@
 
 #include <curses.h>
 
+#include "ext.h"
 #include "recover.h"
 
 #define CURS_HID    0
@@ -23,12 +25,12 @@
 
 #define COLOR_ERROR 1
 
+#define DT_BLK      6
+
 struct win_s {
     WINDOW *win;
     chtype *title;
     int title_len;
-    chtype *message;
-    int message_len;
     int text_w;
     int text_h;
     int cur_x;
@@ -37,12 +39,19 @@ struct win_s {
 
 struct win_s op;
 struct win_s cmds;
-struct win_s pu;
-struct win_s pu_shadow;
+struct win_s err;
+struct win_s err_shadow;
+struct win_s blk_dev;
+struct win_s blk_dev_shadow;
 
 int drive_selected = 0;
 int drive_scanned = 0;
 int files_rebuilt = 0;
+
+char **block_devices_str = 0;
+chtype **block_devices = 0;
+int n_block_devices = 0;
+int block_dev_choice = -1;
 
 /*
  * Turns a char* into a chtype*
@@ -74,11 +83,13 @@ void move_to(struct win_s *w, int x, int y) {
 }
 
 /*
- * Generate a popup for ERROR, WARN, and SCAN
+ * Generate a popup for ERROR and WARN
  */
-void create_popup (enum status_code_e s, const char *fmt, va_list ap) {
+void create_error (enum status_code_e s, const char *fmt, va_list ap) {
     char *title;
-    char message [100];
+    char message_str [100];
+    chtype *message;
+    int message_len;
     int x;
     int y;
     int width;
@@ -90,54 +101,52 @@ void create_popup (enum status_code_e s, const char *fmt, va_list ap) {
         title = "ERROR!";
         attr = attr | A_UNDERLINE;
         pair = COLOR_ERROR;
-    } else if (s == WARN) {
-        title = "Warning!";
     } else {
-        title = "Scanning Drive...";
+        title = "Warning!";
     }
 
-    pu.title_len = strlen(title);
-    pu.title = strchtype(title, pu.title_len);
+    err.title_len = strlen(title);
+    err.title = strchtype(title, err.title_len);
 
     /* Convert the message into it's string form */
-    pu.message_len = sizeof(message) / sizeof(*message);
-    memset(message, 0, pu.message_len);
-    pu.message_len = vsprintf(message, fmt, ap);
-    pu.message = strchtype(message, pu.message_len);
+    message_len = sizeof(message_str) / sizeof(*message_str);
+    memset(message_str, 0, message_len);
+    message_len = vsprintf(message_str, fmt, ap);
+    message = strchtype(message_str, message_len);
 
     /* Center the popup horizontally */
-    width = pu.message_len + 6;
+    width = message_len + 6;
     width = (COLS / 3 > width) ? COLS / 3 : width;
-    pu.text_w = width - 2;
+    err.text_w = width - 2;
     x = (COLS - width) / 2;
 
     /* Center the popup vertically */
     height = LINES / 3;
-    pu.text_h = height - 2;
+    err.text_h = height - 2;
     y = height;
 
     /* Setup shadow */
-    pu_shadow.win = newwin(height, width, y, x + 1);
-    wborder(pu_shadow.win,
+    err_shadow.win = newwin(height, width, y, x + 1);
+    wborder(err_shadow.win,
         SHADOW, SHADOW, SHADOW, SHADOW,
         SHADOW, SHADOW, SHADOW, SHADOW);
 
     /* Set up popup */
-    pu.win = newwin(height, width, y - 1, x);
-    move_to(&pu, (pu.text_w - pu.message_len) / 2, pu.text_h / 3);
-    waddchstr(pu.win, pu.message);
+    err.win = newwin(height, width, y - 1, x);
+    move_to(&err, (err.text_w - message_len) / 2, err.text_h / 3);
+    waddchstr(err.win, message);
     /* Set up border */
-    wborder(pu.win,
+    wborder(err.win,
         /* Left, right, top, bottom sides */
         BOX_VER, BOX_VER, BOX_HOR, BOX_HOR,
         BOX_TL, BOX_TR, BOX_BL, BOX_BR);
-    move_to(&pu, 1, 0);
-    waddchstr(pu.win, pu.title);
-    wbkgd(pu.win, COLOR_PAIR(pair) | A_REVERSE);
-    wchgat(pu.win, pu.title_len, attr, pair, NULL);
+    move_to(&err, 1, 0);
+    waddchstr(err.win, err.title);
+    wbkgd(err.win, COLOR_PAIR(pair) | A_REVERSE);
+    wchgat(err.win, err.title_len, attr, pair, NULL);
 
-    wnoutrefresh(pu_shadow.win);
-    wnoutrefresh(pu.win);
+    wnoutrefresh(err_shadow.win);
+    wnoutrefresh(err.win);
     doupdate();
 
     getch();
@@ -194,7 +203,7 @@ void status (enum status_code_e sl, ...) {
 
     /* Handle error codes */
     case ERROR:
-        create_popup(sl, va_arg(ap, const char*), ap);
+        create_error(sl, va_arg(ap, const char*), ap);
         break;
     case WARN:
         break;
@@ -204,9 +213,216 @@ void status (enum status_code_e sl, ...) {
 }
 
 /*
+ * Popup to choose a block device
+ */
+void block_dev_popup () {
+    const char *title = "Block Device List";
+    const char *inst1_str = "[Up/Down]: Choose";
+    chtype *inst1 = strchtype(inst1_str, strlen(inst1_str));
+    const char *inst2_str = "[Enter]: Confirm";
+    chtype *inst2 = strchtype(inst2_str, strlen(inst2_str));
+    const char *inst3_str = "[Q]: Cancel";
+    chtype *inst3 = strchtype(inst3_str, strlen(inst3_str));
+    int cx;
+    size_t longest_name = 0;
+    int x;
+    int y;
+    unsigned int width;
+    int height;
+    attr_t attr = A_REVERSE;
+
+    blk_dev.title_len = strlen(title);
+    blk_dev.title = strchtype(title, blk_dev.title_len);
+
+    for (cx = 0; cx < n_block_devices; cx++) {
+        /* Convert the names into chtype* */
+        *(block_devices + cx) = strchtype(*(block_devices_str + cx),
+            strlen(*(block_devices_str + cx)));
+
+        /* Get the longest device name */
+        longest_name = (longest_name > strlen(*(block_devices_str + cx)))
+            ? longest_name
+            : strlen(*(block_devices_str + cx));
+    }
+
+    /* Center horizontally */
+    width = longest_name + 6;
+    width = (COLS / 3 > width) ? COLS / 3 : width;
+    width = (strlen(inst1_str) + strlen(inst2_str) + strlen(inst3_str)
+        > width)
+            ? strlen(inst1_str) + strlen(inst2_str) + strlen(inst3_str)
+            : width;
+    blk_dev.text_w = width - 2;
+    x = (COLS - width) / 2;
+
+    /* Center vertically */
+    height = LINES / 3;
+    height = (n_block_devices + 4 > height) ? n_block_devices + 4 : height;
+    blk_dev.text_h = height - 2;
+    y = (LINES - height) / 2;
+
+    /* Setup shadow */
+    blk_dev_shadow.win = newwin(height, width, y, x + 1);
+    wborder(blk_dev_shadow.win,
+        SHADOW, SHADOW, SHADOW, SHADOW,
+        SHADOW, SHADOW, SHADOW, SHADOW);
+
+    /* Setup popup */
+    blk_dev.win = newwin(height, width, y - 1, x);
+    /* Reuse x and y as the top left of the listing */
+    x = (blk_dev.text_w - longest_name) / 2;
+    y = (blk_dev.text_h - n_block_devices) / 2 + 1;
+    /* Print the listing */
+    for (cx = 0; cx < n_block_devices; cx++) {
+        move_to(&blk_dev, x, y + cx);
+        waddchstr(blk_dev.win, *(block_devices + cx));
+    }
+    /* Print instructions */
+    move_to(&blk_dev, 1, y + cx);
+    waddchstr(blk_dev.win, inst1);
+    move_to(&blk_dev,
+        strlen(inst1_str) +
+            (((blk_dev.text_w - (strlen(inst1_str) + strlen(inst3_str))) -
+            strlen(inst2_str)) / 2),
+        y + cx);
+    waddchstr(blk_dev.win, inst2);
+    move_to(&blk_dev, blk_dev.text_w - strlen(inst3_str), y + cx);
+    waddchstr(blk_dev.win, inst3);
+    /* Set up border */
+    wborder(blk_dev.win,
+        /* Left, right, top, bottom sides */
+        BOX_VER, BOX_VER, BOX_HOR, BOX_HOR,
+        BOX_TL, BOX_TR, BOX_BL, BOX_BR);
+    move_to(&blk_dev, 1, 0);
+    waddchstr(blk_dev.win, blk_dev.title);
+    wbkgd(blk_dev.win, attr);
+
+    wnoutrefresh(blk_dev_shadow.win);
+    wnoutrefresh(blk_dev.win);
+    doupdate();
+
+    /* Selection */
+    for(cx = 0; ; ) {
+        wchgat(blk_dev.win, longest_name, attr, 0, NULL);
+        move_to(&blk_dev, x, y + cx);
+        wchgat(blk_dev.win, longest_name, A_NORMAL, 0, NULL);
+        wnoutrefresh(blk_dev.win);
+        doupdate();
+
+        switch(getch()) {
+        case KEY_DOWN:
+            cx++;
+            cx = (cx >= n_block_devices) ? n_block_devices - 1 : cx;
+            break;
+        case KEY_UP:
+            cx--;
+            cx = (cx < 0) ? 0 : cx;
+            break;
+        case '\n':
+            goto select;
+        case 'Q':
+        case 'q':
+            return;
+        default:
+            break;
+        }
+    }
+select:
+    /* Initialize the recovery with the selected device */
+    init(*(block_devices_str + cx));
+}
+
+/*
+ * Find all the block devices under /dev
+ */
+void find_block_devs () {
+    int cx;
+    const char *dname = "/dev/";
+    DIR *dir;
+    struct dirent *de;
+
+    /* Clear the array */
+    if (block_devices_str) {
+        for (cx = 0; cx < n_block_devices; cx++) {
+            free(*(block_devices_str + cx));
+        }
+        free(block_devices_str);
+    }
+    if (block_devices) {
+        for (cx = 0; cx < n_block_devices; cx++) {
+            free(*(block_devices + cx));
+        }
+        free(block_devices);
+    }
+    n_block_devices = 0;
+    block_devices_str = 0;
+    block_devices = 0;
+    block_dev_choice = -1;
+
+    /* Open a stream to /dev */
+    dir = opendir(dname);
+    if (!dir) {
+        status(ERROR, "Unable to open directory: %s", dname);
+        return;
+    }
+
+    /* Go through each entry */
+    for (de = readdir(dir); de; de = readdir(dir)) {
+        /* Test if it is a block device */
+        if (de->d_type == DT_BLK) {
+            n_block_devices++;
+            block_devices_str = realloc(block_devices_str,
+                n_block_devices * sizeof(*block_devices_str));
+            *(block_devices_str + n_block_devices - 1) =
+                malloc(strlen(dname) + strlen(de->d_name));
+            strcpy(*(block_devices_str + n_block_devices - 1), dname);
+            strcpy((*(block_devices_str + n_block_devices - 1) +
+                strlen(dname)), de->d_name);
+        }
+    }
+    block_devices = calloc(n_block_devices, sizeof(*block_devices));
+
+    block_dev_popup();
+
+    closedir(dir);
+}
+
+/*
  * Cleanup tasks for ncurses run on program exit
  */
 void tui_cleanup () {
+    int cx;
+
+    if (block_devices) {
+        for (cx = 0; cx < n_block_devices; cx++) {
+            free(*(block_devices + cx));
+        }
+        free(block_devices);
+    }
+    if (block_devices_str) {
+        for (cx = 0; cx < n_block_devices; cx++) {
+            free(*(block_devices_str + cx));
+        }
+        free(block_devices_str);
+    }
+    if (blk_dev_shadow.win) {
+        delwin(blk_dev_shadow.win);
+    }
+    if (blk_dev.title) {
+        free(blk_dev.title);
+    }
+    if (blk_dev.win) {
+        delwin(blk_dev.win);
+    }
+    if (err_shadow.win) {
+        delwin(err_shadow.win);
+    }
+    if (err.title) {
+        free(err.title);
+    }
+    if (err.win) {
+        delwin(err.win);
+    }
     if (cmds.title) {
         free(cmds.title);
     }
@@ -309,11 +525,13 @@ void prep_op () {
  * Prepare cmds specific things
  */
 void prep_cmds () {
+    int inc = 0;
     const char *drive_prompt_str = "Drive: ";
     chtype *drive_prompt = 0;
     const char *no_drive_msg_str = "[no drive selected]";
     chtype *no_drive_msg = 0;
-    int inc = 0;
+    char drive_stats_str [100];
+    chtype *drive_stats = 0;
     const char *f01_str = "F1: Select Drive";
     chtype *f01 = 0;
     const char *f03_str = "F3: Scan Drive";
@@ -345,6 +563,12 @@ void prep_cmds () {
     move_to(&cmds, cmds.cur_x + strlen(drive_prompt_str), cmds.cur_y);
     /* Actual drive stats if drive is selected */
     if (drive_selected) {
+        sprintf(drive_stats_str,
+            "%s    %u blocks * %u B/block = %u MiB",
+            fs_info.name, *(fs_info.nblocks), BYTES_PER_BLOCK,
+            (*(fs_info.nblocks) >> 10) * (BYTES_PER_BLOCK >> 10));
+        drive_stats = strchtype(drive_stats_str, strlen(drive_stats_str));
+        waddchstr(cmds.win, drive_stats);
     } else {
         waddchstr(cmds.win, no_drive_msg);
     }
@@ -369,6 +593,8 @@ void prep_cmds () {
     /* Disable if drive not scanned */
     if (!drive_scanned) {
         wchgat(cmds.win, strlen(f07_str), A_UNDERLINE, COLOR_ERROR, NULL);
+    } else {
+        wchgat(cmds.win, strlen(f07_str), A_NORMAL, 0, NULL);
     }
     move_to(&cmds, cmds.cur_x + inc, cmds.cur_y);
     waddchstr(cmds.win, f09);
@@ -379,6 +605,27 @@ void prep_cmds () {
     move_to(&cmds, cmds.cur_x + inc, cmds.cur_y);
     waddchstr(cmds.win, f11);
 
+    if (f11) {
+        free(f11);
+    }
+    if (f09) {
+        free(f09);
+    }
+    if (f07) {
+        free(f07);
+    }
+    if (f05) {
+        free(f05);
+    }
+    if (f03) {
+        free(f03);
+    }
+    if (f01) {
+        free(f01);
+    }
+    if (drive_stats) {
+        free(drive_stats);
+    }
     if (no_drive_msg) {
         free(no_drive_msg);
     }
@@ -397,6 +644,7 @@ int parse_input (int key) {
     switch (key) {
     /* Select Drive */
     case KEY_F(1):
+        find_block_devs();
         return 1;
     /* Scan Drive */
     case KEY_F(3):
@@ -409,7 +657,7 @@ int parse_input (int key) {
     /* Scan Results */
     case KEY_F(5):
         /* Error if no drive not scanned */
-        if (!drive_selected) {
+        if (!drive_scanned) {
             status(ERROR, "No drive scanned!");
         } else {
         }
@@ -417,7 +665,7 @@ int parse_input (int key) {
     /* Rebuild Files */
     case KEY_F(7):
         /* Error if no drive not scanned */
-        if (!drive_selected) {
+        if (!drive_scanned) {
             status(ERROR, "No drive scanned!");
         } else {
         }
@@ -425,7 +673,7 @@ int parse_input (int key) {
     /* List Files */
     case KEY_F(9):
         /* Error if no rebuilt files */
-        if (!drive_selected) {
+        if (!files_rebuilt) {
             status(ERROR, "No files have been rebuilt yet!");
         } else {
         }
@@ -449,6 +697,9 @@ int main () {
     tui_init();
 
     do {
+        if (fs_info.name) {
+            drive_selected = 1;
+        }
         /* Prep all the windows */
         wnoutrefresh(stdscr);
         prep_op();
